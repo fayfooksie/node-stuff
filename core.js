cec431bdb27f69a68d8c576ed8c458100d2c7d21
fayfooksie <fayfooksie@gmail.com>
2014-10-30 05:51:02 -0500

    added startup.txt
    default processes are grabbed from startup.txt in the current directory if available instead of requiring a server/processes/ folder

--- core.js
+++ core.js
@@ -32,68 +32,108 @@ console.log=function(initial) {
 cli.prompt();
 cli.on("line", function(data) {
 	data=data.toString().split(/^(\w+) ?/);
-	switch(data[1]) {
+	var	command=data[1];
+	data=data[2];
+	switch(command) {
 		default:
-			console.log(fmt.err("invalid"));
+			console.log(fmt.err("invalid command '%s'"), command);
 			break;
+		case "?":
 		case "help":
 			console.log(fmt.out(
 					"help             this message"+
 				"\r\neval    [code]   evaluate code"+
-				"\r\ncplist           list child processes"+
+				"\r\nexec    [code]   run shell command"+
+				"\r\nlist             list child processes"+
 				"\r\nkill    [id]     kill child process"+
 				"\r\nstart   [id]     create new child process"+
 				"\r\nrestart [id]     restart child process"+
+				"\r\nignore  [id]     toggle logging for process"+
 				"\r\nuse     [id]     select process to send data to"+
 				"\r\nsend    [data]   send data to selected process"
 				));
 			break;
 		case "eval":
 			try {
-				console.log(fmt.out(String(eval(data[2]))));
+				console.log(fmt.out(String(eval(data))));
 				}
 			catch(error) {
 				console.log(fmt.err(error.message));
 				}
 			break;
-		case "cplist":
+		case "exec":
+			cp.exec(data, function(error, stdout, stderr) {
+				if(error) {
+					console.log(fmt.err(error.message));
+					}
+				else {
+					console.log(fmt.out((stdout||stderr).toString()));
+					}
+				});
+			break;
+		case "list":
 			console.log(fmt.out(Object.keys(cps).join("\r\n")));
 			break;
 		case "kill":
-			if(cps[data[2]]) {
-				cps[data[2]].kill();
-				delete cps[data[2]];
-				console.log(fmt.out("killing %s..."), data[2]);
+		case "stop":
+			if(cps[data]) {
+				cps[data].kill();
+				delete cps[data];
+				console.log(fmt.out("killing %s..."), data);
 				}
 			else {
 				console.log(fmt.err("no such process"));
 				}
 			break;
+		case "node":
+		case "load":
 		case "start":
-			spawn(data[2]);
+			if(cps[data]) {
+				console.log(fmt.err("already running %s"), data);
+				}
+			else {
+				spawn(data);
+				}
 			break;
+		case "reload":
 		case "restart":
-			if(cps[data[2]]) {
-				cps[data[2]].kill();
-				console.log(fmt.out("killing %s..."), data[2]);
-				spawn(data[2]);
+			if(cps[data]) {
+				var	reuse=usecp===cps[data];
+				cps[data].kill();
+				console.log(fmt.out("killing %s..."), data);
+				spawn(cps[data]._path);
+				if(reuse) {
+					usecp=cps[data];
+					}
 				}
 			else {
 				console.log(fmt.err("no such process"));
+				}
+			break;
+		case "ignore":
+		case "silence":
+			if(ignore[data]) {
+				ignore[data]=0;
+				console.log(fmt.out("%s logging on"), data);
+				}
+			else {
+				ignore[data]=1;
+				console.log(fmt.out("%s logging off"), data);
 				}
 			break;
 		case "use":
-			if(cps[data[2]]) {
-				usecp=cps[data[2]];
-				console.log(fmt.out("using %s"), data[2]);
+		case "select":
+			if(cps[data]) {
+				usecp=cps[data];
+				console.log(fmt.out("using %s"), data);
 				}
 			else {
 				console.log(fmt.err("no such process"));
 				}
 			break;
 		case "send":
-			usecp.send(data[2]);
-			console.log(fmt.out("cp > %s"), data[2]);
+			usecp.send(data);
+			console.log(fmt.out("cp > %s"), data);
 			break;
 		}
 	console.log();
@@ -102,11 +142,14 @@ cli.on("line", function(data) {
 
 //processes
 var	cps={},
-	usecp=null;
-function spawn(id) {
+	usecp=null,
+	ignore={};
+function spawn(path) {
+	var	id=path.replace(/.+\//, "");
 	console.log(fmt.out("loading %s..."), id);
-	cps[id]=cp.fork("server/processes/"+id);
+	cps[id]=cp.fork(path);
 	cps[id]._name=id;
+	cps[id]._path=path;
 	cps[id].on("exit", event_exit);
 	cps[id].on("error", event_error);
 	cps[id].on("message", event_message);
@@ -125,15 +168,20 @@ function event_error(error) {
 	console.log();
 	};
 function event_message(data) {
-	data[0]=fmt.out(data[0]);
-	console.log(fmt.cp_out, this._name);
-	console.log.apply(console, data);
-	console.log();
-	};
-fs.readdir("server/processes", function(error, files) {
-	for(var i=0; i<files.length; ++i) {
-		spawn(files[i]);
+	if(!ignore[this._name]) {
+		data[0]=fmt.out(data[0]);
+		console.log(fmt.cp_out, this._name);
+		console.log.apply(console, data);
+		console.log();
 		}
-	usecp=cps[files[0]];
-	console.log();
+	};
+fs.readFile("startup.txt", function(error, data) {
+	if(!error) {
+		data=data.toString().split(/\r?\n/);
+		for(var i=0; i<data.length; ++i) {
+			spawn(data[i]);
+			}
+		usecp=cps[data[0]];
+		console.log();
+		}
 	});
